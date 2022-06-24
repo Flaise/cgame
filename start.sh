5b5d837bef472218ea02d5c517195a86c34817a4
Flaise <x.Flaise@gmail.com>
2022-06-24 16:54:35 -0400

    Showing compilation time from shell script.

--- start.sh
+++ start.sh
@@ -2,7 +2,8 @@
 mkdir -p ./out/ || exit 1
 
 cd src || exit 1 # removes extraneous folder name from log messages
-c99 -o ../out/cgame *.c `pkg-config --cflags --libs sdl2` || exit 1
+/usr/bin/time -f "completed in %es" \
+    c99 -o ../out/cgame *.c `pkg-config --cflags --libs sdl2` || exit 1
 cd .. || exit 1
 
 ./out/cgame
