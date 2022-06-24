85919591ce602046c893e51c07214c572d540722
Flaise <x.Flaise@gmail.com>
2022-06-24 17:31:39 -0400

    Showing the main window that exits on user input.

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+
 #ifdef _WIN32a
 #include "SDL.h"
 #else
@@ -6,6 +8,31 @@
 
 #include "logging.h"
 
+typedef enum {
+    Proceed,
+    Exit,
+    Error,
+} Status;
+
+Status next_event() {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT:
+                return Exit;
+        }
+    }
+    return Proceed;
+}
+
+Status all_events() {
+    while (true) {
+        Status stat = next_event();
+        if (stat != Proceed) {
+            return stat;
+        }
+    }
+}
 
 int show_window() {
     SDL_Window* win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
@@ -16,15 +43,13 @@ int show_window() {
 
     ERROR("Test error message.");
     INFO("It works?");
+    all_events();
 
     SDL_DestroyWindow(win);
     return 0;
 }
 
-
 int main(int argc, char* argv[]) {
-    SDL_LogSetAllPriority(SDL_LOG_PRIORITY_INFO);
-
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
         ERROR("Unable to initialize SDL.");
         return 1;
