8df42601429d39ba086875e35d32c89a79cda365
Flaise <x.Flaise@gmail.com>
2022-06-24 18:25:17 -0400

    Minor troubleshooting.

--- src/main.c
+++ src/main.c
@@ -35,22 +35,26 @@ Status all_events() {
 }
 
 int show_window() {
-    SDL_Window* win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    SDL_Window* win = SDL_CreateWindow("Window be here", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
     if (win == NULL) {
         ERROR("Unable to create window.");
     	return 1;
     }
 
-    ERROR("Test error message.");
-    INFO("It works?");
     all_events();
 
     SDL_DestroyWindow(win);
     return 0;
 }
 
+/*
+ apparently SDL_CreateWindow still works if SDL_Init is omitted
+ SDL_Init doesn't error out if called twice
+ SDL_CreateWindow still works after SDL_Quit
+ */
+
 int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         ERROR("Unable to initialize SDL.");
         return 1;
     }
