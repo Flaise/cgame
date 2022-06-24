c2bf6f001961b2b3cdee542f736d67155ea367b2
Flaise <x.Flaise@gmail.com>
2022-06-24 10:37:09 -0400

    Adjusted logging style.

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 #ifdef _WIN32a
 #include "SDL.h"
 #else
-#include<linux/time.h>
+#include <linux/time.h>
 #define __timespec_defined 1
 #define __timeval_defined 1
 #define __itimerspec_defined 1
@@ -11,11 +11,19 @@
 #endif
 
 
-int main(int argc, char* argv[])
-{
-    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0) {
-        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+int main(int argc, char* argv[]) {
+    SDL_LogSetAllPriority(SDL_LOG_PRIORITY_INFO);
+
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Unable to initialize SDL: %s", SDL_GetError());
         return 1;
+    }
+
+    SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    if (win == NULL) {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Unable to create window: %s", SDL_GetError());
+    	SDL_Quit();
+    	return 1;
     }
 
     SDL_Log("It works?");
