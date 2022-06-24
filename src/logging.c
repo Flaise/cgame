a82e1ba5ab5b83d7bd1a6a6aa588d885d4f6f311
Flaise <x.Flaise@gmail.com>
2022-06-24 17:04:40 -0400

    Refactored main function, removed unnecessary import.

--- src/logging.c
+++ src/logging.c
@@ -3,10 +3,6 @@
 #ifdef _WIN32a
 #include "SDL.h"
 #else
-#include <linux/time.h>
-#define __timespec_defined 1
-#define __timeval_defined 1
-#define __itimerspec_defined 1
 #include "SDL2/SDL.h"
 #endif
 
--- src/main.c
+++ src/main.c
@@ -1,14 +1,26 @@
 #ifdef _WIN32a
 #include "SDL.h"
 #else
-#include <linux/time.h>
-#define __timespec_defined 1
-#define __timeval_defined 1
-#define __itimerspec_defined 1
 #include "SDL2/SDL.h"
 #endif
 
 #include "logging.h"
+
+
+int show_window() {
+    SDL_Window* win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    if (win == NULL) {
+        ERROR("Unable to create window.");
+    	return 1;
+    }
+
+    ERROR("Test error message.");
+    INFO("It works?");
+
+    SDL_DestroyWindow(win);
+    return 0;
+}
+
 
 int main(int argc, char* argv[]) {
     SDL_LogSetAllPriority(SDL_LOG_PRIORITY_INFO);
@@ -18,17 +30,8 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-    if (win == NULL) {
-        ERROR("Unable to create window.");
-    	SDL_Quit();
-    	return 1;
-    }
-
-    ERROR("Test error message.");
-    INFO("It works?");
-
+    int status = show_window();
+    
     SDL_Quit();
-
-    return 0;
+    return status;
 }
