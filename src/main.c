ec58920054959ffa3488b0984ef5bc11d623b0df
Flaise <x.Flaise@gmail.com>
2022-06-25 13:02:31 -0400

    Embedding and loading image.

--- src/main.c
+++ src/main.c
@@ -9,6 +9,9 @@
 
 #include "logging.h"
 
+#include "res/floor.h"
+#define FLOOR __res_Floor___Grass_1_64x64_png
+
 typedef enum {
     Proceed,
     Exit,
@@ -63,6 +66,12 @@ int run_window() {
     	return 1;
     }
 
+    SDL_Surface* winsurf = SDL_GetWindowSurface(win);
+    if (winsurf == NULL) {
+        ERROR("SDL_GetWindowSurface");
+        return 1;
+    }
+
     int status = all_pending_events();
 
     SDL_DestroyWindow(win);
@@ -76,8 +85,20 @@ int run_window() {
  */
 
 int main(int argc, char* argv[]) {
-    if (IMG_Init(IMG_INIT_PNG) != IMG_INIT_PNG) {
-        ERROR("IMG_Init (png)");
+
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+        ERROR("IMG_Init: png");
+        return 1;
+    }
+
+    printf("size=%ld\n", sizeof(FLOOR));
+
+    SDL_RWops* floor_rw = SDL_RWFromConstMem(FLOOR, sizeof(FLOOR));
+
+    SDL_Surface* floor;
+    floor = IMG_LoadTyped_RW(floor_rw, true, "PNG");
+    if (!floor) {
+        ERROR("IMG_Load_RW: floor");
         return 1;
     }
 
--- start.sh
+++ start.sh
@@ -1,9 +1,20 @@
 
-mkdir -p ./out/ || exit 1
+# CONVERT PNG TO C
+
+mkdir -p ./src/res || exit 1
+xxd --include "./res/Floor - Grass 1 64x64.png" ./src/res/floor.h
+
+
+# COMPILE C
+
+mkdir -p ./bin/ || exit 1
 
 cd src || exit 1 # removes extraneous folder name from log messages
-/usr/bin/time -f "completed in %es" \
-    c99 -o ../out/cgame *.c `pkg-config --cflags --libs sdl2 SDL2_image` || exit 1
+/usr/bin/time -f "compiled in %es" \
+    c99 -o ../bin/cgame *.c `pkg-config --cflags --libs sdl2 SDL2_image` || exit 1
 cd .. || exit 1
 
-./out/cgame
+
+# EXECUTE
+
+./bin/cgame
