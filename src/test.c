8108ef4478f532e529db96ccce6d08a1804360a7
Flaise <x.Flaise@gmail.com>
2022-06-28 23:51:50 -0400

    Fixed tests so they test entity deletion and aren't redundant.

--- src/test.c
+++ src/test.c
@@ -115,36 +115,67 @@ static char* test_compbgone64() {
 }
 
 static char* test_entbgone1() {
-    CompGroup groupint = compgroup_init(3, sizeof(CompInt));
-    CompInt* comp = (CompInt*)component_init(&groupint, 1);
-    mu_assert(comp != NULL, "");
-    mu_assert(groupint.alive == 1, "");
+    CompGroup groups[2] = {
+        compgroup_init(3, sizeof(CompInt)),
+        compgroup_init(3, sizeof(CompInt)),
+    };
+    CompGroup* groupa = &groups[0];
+    CompGroup* groupb = &groups[1];
     
-    component_end(&groupint, 1);
-    mu_assert(groupint.alive == 0, "");
+    CompInt* comp = (CompInt*)component_init(groupa, 1);
+    mu_assert(comp != NULL, "");
+    mu_assert(groupa->alive == 1, "");
+
+    comp = (CompInt*)component_init(groupb, 1);
+    mu_assert(comp != NULL, "");
+    mu_assert(groupb->alive == 1, "");
+
+    groups_entity_end((CompGroup*)groups, 2, 1);
+    mu_assert(groupa->alive == 0, "");
+    mu_assert(groupb->alive == 0, "");
     
     return 0;
 }
 
 static char* test_entbgone2() {
-    CompGroup groupint = compgroup_init(3, sizeof(CompInt));
-
-    CompInt* comp = (CompInt*)component_init(&groupint, 1);
+    CompGroup groups[2] = {
+        compgroup_init(3, sizeof(CompInt)),
+        compgroup_init(3, sizeof(CompInt)),
+    };
+    CompGroup* groupa = &groups[0];
+    CompGroup* groupb = &groups[1];
+    
+    CompInt* comp = (CompInt*)component_init(groupa, 1);
     mu_assert(comp != NULL, "");
-    mu_assert(groupint.alive == 1, "");
-    comp->val = 3;
-
-    comp = (CompInt*)component_init(&groupint, 2);
+    mu_assert(groupa->alive == 1, "");
+    comp->val = 7;
+    
+    comp = (CompInt*)component_init(groupa, 2);
     mu_assert(comp != NULL, "");
-    mu_assert(groupint.alive == 2, "");
-    comp->val = 99;
+    mu_assert(groupa->alive == 2, "");
+    comp->val = 8;
 
-    component_end(&groupint, 1);
-    mu_assert(groupint.alive == 1, "");
+    comp = (CompInt*)component_init(groupb, 1);
+    mu_assert(comp != NULL, "");
+    mu_assert(groupb->alive == 1, "");
+    comp->val = 9;
+    
+    comp = (CompInt*)component_init(groupb, 2);
+    mu_assert(comp != NULL, "");
+    mu_assert(groupb->alive == 2, "");
+    comp->val = 10;
 
-    comp = (CompInt*)groupint.mem;
+    groups_entity_end((CompGroup*)groups, 2, 1);
+    mu_assert(groupa->alive == 1, "");
+    mu_assert(groupb->alive == 1, "");
+    
+    comp = (CompInt*)groupa->mem;
     mu_assert(comp->entity == 2, "");
-    mu_assert(comp->val == 99, "");
+    mu_assert(comp->val == 8, "");
+    
+    comp = (CompInt*)groupb->mem;
+    mu_assert(comp->entity == 2, "");
+    mu_assert(comp->val == 10, "");
     
     return 0;
 }
